76dbc92e9f3a8530ba5d18fac507b4c1c615b867
YipengLi李依朋 <65066990+liyipeng-moon@users.noreply.github.com>
2025-12-04 17:22:03 +0800

    update preprocess

--- Preprocess/Util/PostProcess_function.m
+++ Preprocess/Util/PostProcess_function.m
@@ -12,7 +12,7 @@ high_line_time2 = global_params.high_line_time2;
 
 good_idx = 1;
 GoodUnitStrc = UnitStrc;
-GoodUnitStrc(good_idx).Raster = [];
+
 trial_valid_idx = meta_data.trial_valid_idx;
 onset_time_ms = meta_data.onset_time_ms;
 img_size = meta_data.img_size;
@@ -22,11 +22,14 @@ img_idx = trial_valid_idx(good_trial);
 load("processed\fscale.mat")
 
 template_bc = fscale*readNPY(fullfile('processed/BC/templates._bc_rawWaveforms.npy'));
+template_ks = readNPY(fullfile('kilosort_def_5block_97/templates.npy'));
+template_ks = permute(template_ks,[1,3,2]);
 
 for spike_num = 1:length(UnitStrc)
     spike_time = UnitStrc(spike_num).spiketime_ms;
     psth_range = -pre_onset:post_onset;
     raster_raw = zeros([length(good_trial), pre_onset+post_onset]);
+
     for good_trial_idx = 1:length(good_trial)
         loc_in_orig = good_trial(good_trial_idx);
         onset_time_trial = onset_time_ms(loc_in_orig);
@@ -36,11 +39,13 @@ for spike_num = 1:length(UnitStrc)
             raster_raw(good_trial_idx,floor(time_bound(time_bound_idx)))=raster_raw(good_trial_idx,floor(time_bound(time_bound_idx)))+1;
         end
     end
+
     onset_t = zeros([1, img_size]);
     for img = 1:img_size
         onset_t(img) = sum(img_idx==img);
     end
     psth_raw = zeros(size(raster_raw));
+
     for time_points = 1:size(psth_raw,2)
         if(time_points-psth_window_size_ms/2<1)
             time_window = 1:psth_window_size_ms;
@@ -51,23 +56,29 @@ for spike_num = 1:length(UnitStrc)
         end
         psth_raw(:,time_points) =1000*sum(raster_raw(:, time_window),2)/length(time_window);
     end
+
     response_matrix_img = zeros([img_size, pre_onset+post_onset]);
     for img = 1:img_size
         response_matrix_img(img,:) = sum(psth_raw(img_idx==img, :),1)./ onset_t(img);
     end
-    baseline = sum(raster_raw(:,(base_line_time)+pre_onset+1),2);
-    highline1 = sum(raster_raw(:,(high_line_time1)+pre_onset+1),2);
-    highline2 = sum(raster_raw(:,(high_line_time2)+pre_onset+1),2);
+
+    baseline = mean(raster_raw(:,(base_line_time)+pre_onset+1),2);
+    highline1 = mean(raster_raw(:,(high_line_time1)+pre_onset+1),2);
+    highline2 = mean(raster_raw(:,(high_line_time2)+pre_onset+1),2);
 
     [p1,~,~] = ranksum(highline1,baseline,method="approximate");
     [p2,~,~] = ranksum(highline2,baseline,method="approximate");
 
-
     if( min(p1,p2) < 0.001 && unitType(spike_num)~=0)
+
         wf = squeeze(template_bc(spike_num,:,:));
-        [cc,ww] = prune_wf(wf);
-        GoodUnitStrc(good_idx).waveform = ww;
-        GoodUnitStrc(good_idx).waveformchan = cc;
+        [~,ww] = prune_wf(wf);
+        GoodUnitStrc(good_idx).waveformBC = ww;
+
+        wf = squeeze(template_ks(spike_num,:,:));
+        [~,ww] = prune_wf(wf);
+        GoodUnitStrc(good_idx).waveformKS = ww;
+
         GoodUnitStrc(good_idx).KSidx = spike_num;
 
         GoodUnitStrc(good_idx).spiketime_ms = UnitStrc(spike_num).spiketime_ms;
@@ -83,9 +94,11 @@ for spike_num = 1:length(UnitStrc)
     end
     fprintf('%d good, look %d in %d \n', good_idx-1,spike_num,length(UnitStrc))
 end
+
 GoodUnitStrc(good_idx:end)=[];
 global_params.PsthRange = psth_range(2:end);
 
 file_name_LOCAL = fullfile('processed',sprintf('GoodUnit_%s_g%s.mat',meta_file(1).name(13:end-7), meta_data.g_number));
 save(file_name_LOCAL, "GoodUnitStrc", "trial_ML","global_params",'meta_data','-v7.3')
+
 end
--- Preprocess/Util/PostProcess_function_raw.m
+++ Preprocess/Util/PostProcess_function_raw.m
@@ -11,9 +11,17 @@ trial_ML = load(fullfile('processed',ML_FILE(1).name)).trial_ML;
 [qMetric, unitType] = run_bc(data_path);
 [UnitStrc] = load_KS4_output('./kilosort_def_5block_97',IMEC_AP_META,SyncLine);
 
+
 for trial_idx = 1:length(trial_ML)
     trial_ML(trial_idx).AnalogData.Mouse=[];
     trial_ML(trial_idx).AnalogData.KeyInput=[];
+end
+
+if(strcmp(pwd,'F:\NSD_FULL_RAW\240709'))
+    UnitStrc(638)=[];
+end
+if(strcmp(pwd,'F:\NSD_FULL_RAW\240924'))
+    UnitStrc(328)=[];
 end
 
 file_name_LOCAL = fullfile('processed',sprintf('GoodUnitRaw_%s_g%s.mat',meta_file(1).name(6:end-4), meta_data.g_number));
